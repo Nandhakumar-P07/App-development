772bb63abe7d427f6543289b923d9a0424ae519e
Nandhakumar-P07 <nandhakumarja9@gmail.com>
2023-10-11 11:46:08 +0530

    day17 commit

---
+++ Day17/freelancerfinderapp/src/main/java/com/nandhakumar/freelancerfinderapp/Models/enumerate/Role.java
@@ -0,0 +1,11 @@
+package com.nandhakumar.freelancerfinderapp.Models.enumerate;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Role {
+    ADMIN,
+    CLIENT,
+    FREELANCER
+}
+
