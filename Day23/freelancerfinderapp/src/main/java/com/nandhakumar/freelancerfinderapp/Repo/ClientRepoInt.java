43e8306d2b261d83549d01d5bd6a9378d8f2da99
Nandhakumar-P07 <nandhakumarja9@gmail.com>
2023-10-16 10:05:53 +0530

    day23 commit

---
+++ Day23/freelancerfinderapp/src/main/java/com/nandhakumar/freelancerfinderapp/Repo/ClientRepoInt.java
@@ -0,0 +1,16 @@
+package com.nandhakumar.freelancerfinderapp.Repo;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nandhakumar.freelancerfinderapp.Models.ClientModel;
+
+@Repository
+public interface ClientRepoInt extends JpaRepository<ClientModel,Integer> {
+    
+    Optional<ClientModel> findByEmail(String email);
+
+    ClientModel findById(int id);
+}
